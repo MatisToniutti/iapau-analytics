48b1f4bc99145a850dda93ab906c914286b6232b
MatisToniutti <47952310+MatisToniutti@users.noreply.github.com>
2023-05-29 12:21:46 +0200

    fix json parameter is now code instead of filename

--- Serveur.java
+++ Serveur.java
@@ -1,3 +1,5 @@
+
+//il faut lancer le serveur avec java -cp .:json.jar Serveur
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -12,10 +14,11 @@ import java.util.logging.Logger;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.FileReader;
+import java.io.StringReader;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
+import org.json.JSONObject;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -24,17 +27,18 @@ import java.nio.file.Paths;
 
 public class Serveur {
     // logger pour trace
-    private static final Logger LOGGER = Logger.getLogger( Serveur.class.getName() );
+    private static final Logger LOGGER = Logger.getLogger(Serveur.class.getName());
     private static final String SERVEUR = "localhost"; // url de base du service
     private static final int PORT = 8001; // port serveur
     private static final String URL = "/test"; // url de base du service
     // boucle principale qui lance le serveur sur le port 8001, à l'url test
+
     public static void main(String[] args) {
         HttpServer server = null;
         try {
             server = HttpServer.create(new InetSocketAddress(SERVEUR, PORT), 0);
 
-            server.createContext(URL, new  MyHttpHandler());
+            server.createContext(URL, new MyHttpHandler());
             ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
             server.setExecutor(threadPoolExecutor);
             server.start();
@@ -45,22 +49,10 @@ public class Serveur {
         }
     }
 
-    public static String readFileContent(String filePath) throws IOException {
-        StringBuilder stringBuilder = new StringBuilder();
-
-        try (BufferedReader reader = Files.newBufferedReader(Paths.get(filePath))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                stringBuilder.append(line);
-            }
-        }
-
-        return stringBuilder.toString();
-    }
-
     private static class MyHttpHandler implements HttpHandler {
         /**
          * Manage GET request param
+         * 
          * @param httpExchange
          * @return first value
          */
@@ -71,69 +63,67 @@ public class Serveur {
                     .split("=")[1];
         }
 
-        private String occurencesNumbers(List<String> words, String filename){
-            // A faire : récupérer le texte du fichier, conmpter le nombre d'occurences
+        private String occurencesNumbers(List<String> words, String code) {
             String response = "{";
-            try {
-                String fileContent = readFileContent(filename);
-                for(String word : words){
-                    response += "\"" +word + "\":";
-                    response += Integer.toString(fileContent.split(word, -1).length - 1) + ",";
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+            for (String word : words) {
+                response += "\"" + word + "\":";
+                response += Integer.toString(code.split(word, -1).length - 1) + ",";
             }
-            return response.substring(0, response.length() - 1)+"}";
+            return response.substring(0, response.length() - 1) + "}";
         }
 
-        private String countFunctionsLines(String filename){
+        private String countFunctionsLines(String code) {
             int countLines = 0;
             int countFunctions = 0;
             int currentFunctionLines = 0;
             int functionLines = 0;
-            int indentationCount=0;
-            int indentationFunction=0;
+            int indentationCount = 0;
+            int indentationFunction = 0;
+            code = code.replace("\"", "");
             List<Integer> functionLineCounts = new ArrayList<>();
-            try (BufferedReader fileReader = new BufferedReader(new FileReader(filename))) {
-                String fileLine;
-                String fileLineNotStrip;
+            try (BufferedReader strReader = new BufferedReader(new StringReader(code))) {
+                String line;
+                String lineNotStrip;
                 Boolean function = false;
-                while ((fileLineNotStrip = fileReader.readLine()) != null) {
-                    
-                    fileLine = fileLineNotStrip.strip();
+                while ((lineNotStrip = strReader.readLine()) != null) {
 
-                    if (!fileLine.isEmpty() && !fileLine.startsWith("#")) {
+                    line = lineNotStrip.strip();
+
+                    if (!line.isEmpty() && !line.startsWith("#")) {
                         countLines++;
-                        indentationCount=0;
-                        while (indentationCount < fileLineNotStrip.length() && (fileLineNotStrip.charAt(indentationCount) == ' ' || fileLineNotStrip.charAt(indentationCount) == '\t')) {
+                        indentationCount = 0;
+                        while (indentationCount < lineNotStrip.length() && (lineNotStrip.charAt(indentationCount) == ' '
+                                || lineNotStrip.charAt(indentationCount) == '\t')) {
                             indentationCount++;
                         }
-                        if(function && indentationCount==indentationFunction &&currentFunctionLines > 1){
+                        if (function && indentationCount == indentationFunction && currentFunctionLines > 1) {
                             functionLineCounts.add(currentFunctionLines - 1);
                             functionLines += currentFunctionLines - 1;
                             currentFunctionLines = 0;
                             function = false;
-                        }else{
+                        } else {
                             currentFunctionLines++;
                         }
-                        
-                        if (fileLine.startsWith("def ")) {
+
+                        if (line.startsWith("def ")) {
                             countFunctions++;
-                            indentationFunction=0;
+                            indentationFunction = 0;
                             function = true;
-                            while (indentationFunction < fileLineNotStrip.length() && (fileLineNotStrip.charAt(indentationFunction) == ' ' || fileLineNotStrip.charAt(indentationFunction) == '\t')) {
+                            while (indentationFunction < lineNotStrip.length()
+                                    && (lineNotStrip.charAt(indentationFunction) == ' '
+                                            || lineNotStrip.charAt(indentationFunction) == '\t')) {
                                 indentationFunction++;
                             }
                             if (currentFunctionLines > 1) {
                                 functionLineCounts.add(currentFunctionLines - 1);
                                 functionLines += currentFunctionLines - 1;
                             }
-                            
+
                             currentFunctionLines = 1;
                         }
                     }
                 }
-                
+
                 // Compte la dernière fonction
                 if (function && currentFunctionLines > 0) {
                     functionLineCounts.add(currentFunctionLines);
@@ -148,10 +138,12 @@ public class Serveur {
                 sum += value;
                 nb++;
             }
-            double mean = sum/nb;
-            return "{ \"countLines\": " + countLines + ", \"countFunctions\": " + countFunctions + ", \"meanFunctionLines\": "+ mean + ", \"maxFunctionLines\": " + Collections.max(functionLineCounts) + ", \"minFunctionLines\": " + Collections.min(functionLineCounts) + " }";
+            double mean = sum / nb;
+            return "{ \"countLines\": " + countLines + ", \"countFunctions\": " + countFunctions
+                    + ", \"meanFunctionLines\": " + mean + ", \"maxFunctionLines\": "
+                    + Collections.max(functionLineCounts) + ", \"minFunctionLines\": "
+                    + Collections.min(functionLineCounts) + " }";
         }
-
 
         private String handlePostRequest(HttpExchange httpExchange) {
             InputStream requestBody = httpExchange.getRequestBody();
@@ -160,6 +152,7 @@ public class Serveur {
             String countFunctionsandLines = "";
             String occurencesNumbers = "";
             String line;
+            String code = "";
             List<String> words = new ArrayList<>();
             try {
                 while ((line = reader.readLine()) != null) {
@@ -170,41 +163,28 @@ public class Serveur {
             }
             // Analyse des données du corps de la requête
             String body = stringBuilder.toString();
-            String cleanedJsonString = body.substring(1, body.length() - 1);
+            JSONObject jsonObject = new JSONObject(body);
 
-            // Split the cleaned JSON string into key-value pairs
-            String[] keyValuePairs = cleanedJsonString.split(",");
-            String filename = "";
-
-            // Iterate over the key-value pairs
-            for (String keyValuePair : keyValuePairs) {
-                // Split each key-value pair into key and value
-                String[] parts = keyValuePair.split(":");
-                String key = parts[0].trim();
-                String value = parts[1].trim();
-
-                // Remove surrounding quotes from the value
-                Pattern pattern = Pattern.compile("^\"(.*)\"$");
-                Matcher matcher = pattern.matcher(value);
-                key = key.replaceAll("\"","");
-                if (matcher.matches()) {
-                    value = matcher.group(1);
-                }
-                if(key.equals("filename")){
-                    filename = value;
-                }else{
+            // Iterate over the keys and values
+            for (String key : jsonObject.keySet()) {
+                String value = jsonObject.getString(key);
+                if (key.equals("code")) {
+                    code = value; // Restaurer les sauts de ligne
+                } else {
                     words.add(value);
                 }
             }
-            return "{ \"countFunctionsandLines\": " + countFunctionsLines(filename)  +  ", \"occurencesNumbers\": " + occurencesNumbers(words,filename) + " }";
+            return "{ \"countFunctionsandLines\": " + countFunctionsLines(code) + ", \"occurencesNumbers\":"
+                    + occurencesNumbers(words, code) + " }";
         }
 
-        /** 
+        /**
          * Generate simple response html page
+         * 
          * @param httpExchange
          * @param requestParamVaue
          */
-        private void handleResponse(HttpExchange httpExchange, String requestParamValue)  throws  IOException {
+        private void handleResponse(HttpExchange httpExchange, String requestParamValue) throws IOException {
             OutputStream outputStream = httpExchange.getResponseBody();
             // this line is a must
             httpExchange.sendResponseHeaders(200, requestParamValue.length());
@@ -216,14 +196,13 @@ public class Serveur {
         // Interface method to be implemented
         @Override
         public void handle(HttpExchange httpExchange) throws IOException {
-            String requestParamValue=null;
-            if("GET".equals(httpExchange.getRequestMethod())) {
+            String requestParamValue = null;
+            if ("GET".equals(httpExchange.getRequestMethod())) {
                 requestParamValue = handleGetRequest(httpExchange);
-            }
-            else if("POST".equals(httpExchange.getRequestMethod())) {
+            } else if ("POST".equals(httpExchange.getRequestMethod())) {
                 requestParamValue = handlePostRequest(httpExchange);
             }
-            handleResponse(httpExchange,requestParamValue);
+            handleResponse(httpExchange, requestParamValue);
 
         }
     }
