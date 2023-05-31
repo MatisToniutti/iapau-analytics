3cf037f36685b5f6bf5840e8e100df499202658c
hcaumeil <78665596+hcaumeil@users.noreply.github.com>
2023-06-01 00:09:42 +0200

    Cringing is now a wii sport

--- Serveur.java
+++ Serveur.java
@@ -28,7 +28,7 @@ import java.nio.file.Paths;
 public class Serveur {
     // logger pour trace
     private static final Logger LOGGER = Logger.getLogger(Serveur.class.getName());
-    private static final String SERVEUR = "localhost"; // url de base du service
+    private static final String SERVEUR = "0.0.0.0"; // url de base du service
     private static final int PORT = 8080; // port serveur
     private static final String URL = "/"; // url de base du service
     // boucle principale qui lance le serveur sur le port 8080, à l'url test
